f0202ae9d99a9b94054b1d15fb399226cce9ceb2
Mariana Arboleda <marianaarboleda@uninorte.edu.co>
2026-07-17 11:03:26 -0500

    Update behaviors.ts

--- behaviors.ts
+++ behaviors.ts
@@ -306,7 +306,8 @@ namespace roversaPetBot {
                 "Konnichiwa"
             ]
             basic.showString("" + (greetings._pickRandom()))
-            music.play(music.builtInPlayableMelody(Melodies.Ringtone), music.PlaybackMode.InBackground)
+            music.play(music.builtInPlayableMelody(Melodies.Ringtone), 
+            music.PlaybackMode.UntilDone)
             basic.showIcon(IconNames.SmallHeart)
             basic.pause(2000)
             music.stopAllSounds()
